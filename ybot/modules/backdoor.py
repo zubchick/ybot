98c46e9f5b3736ad62f0076301fef7ac8aa95861
Nikita Zubkov <zubchick@yandex-team.ru>
2015-10-31 21:31:54 +0300

    backdoor module configuration

--- ybot/modules/backdoor.py
+++ ybot/modules/backdoor.py
@@ -1,10 +1,14 @@
 # coding: utf-8
 from ybot.events import emitter
+from ybot.conf import settings
 from gevent.backdoor import BackdoorServer
 
-server = BackdoorServer(('127.0.0.1', 5001),
+port = settings[__name__].get('port', 5001)
+
+server = BackdoorServer(('127.0.0.1', port),
                         banner="Hello from gevent backdoor!",
                         locals={})
+
 
 @emitter()
 def backdoor():
