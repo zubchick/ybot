d429adf133d8611ec2bbcfd4e2d0d6a3b8f43b8d
Nikita Zubkov <zubchick@yandex-team.ru>
2015-11-08 16:30:15 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
 # coding: utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name='ybot',
+    version='0.1',
+    description='Extensible telegram bot',
+
+    author='Nikita Zubkov',
+    author_email='zubchick@yandex.ru',
+    packages=find_packages(),
+    install_requires=['gevent>=1.0.2', 'PyYAML>=3.11'],
+
+    entry_points={
+        'console_scripts': [
+            'ybot = ybot:main',
+        ],
+    },
+)
