d01a1e5da4d63c93e0725f3d02ff15ce87e7aa9b
Nikita Zubkov <zubchick@yandex-team.ru>
2015-11-08 16:31:14 +0300

    telegram module improvements

--- ybot/modules/telegram.py
+++ ybot/modules/telegram.py
@@ -1,12 +1,15 @@
 # coding: utf-8
 from __future__ import absolute_import
+import logging
 import re
+import gevent
 import telegram
 from ybot.events import emitter, splitter
 from ybot.conf import settings
 
 conf = settings[__name__]     # 'ybot.modules.telegram'
 bot = telegram.Bot(token=conf['token'])
+log = logging.getLogger(__name__)
 
 
 @emitter('ybot.telegram.message')
@@ -15,17 +18,34 @@ def updates():
     offset = None
 
     while True:
-        updates = bot.getUpdates(offset=offset, timeout=timeout) or []
+        try:
+            updates = bot.getUpdates(offset=offset, timeout=timeout) or []
+        except telegram.TelegramError:
+            log.exception('Telegram error')
+            continue
+
         for update in updates:
             offset = update.update_id + 1
             if update.message:
                 yield update.message
 
 
-@splitter(['ybot.telegram.message'], ['ybot.telegram.command'])
+@splitter(
+    ['ybot.telegram.message'],
+    [
+        'ybot.telegram.command',
+        'ybot.telegram.new_chat_participant',
+        'ybot.telegram.left_chat_participant',
+    ],
+)
 def command_splitter(name, value):
     bot_name = conf.get('bot_name', r'\w{1,32}')
     pattern = r'^/\w+(:?@{name})?( .*)?$'.format(name=bot_name)
 
     if re.match(pattern, value.text):
         yield ('ybot.telegram.command', value)
+
+    if value.new_chat_participant is not None:
+        yield ('ybot.telegram.new_chat_participant', value)
+    elif value.left_chat_participant is not None:
+        yield ('ybot.telegram.left_chat_participant', value)
