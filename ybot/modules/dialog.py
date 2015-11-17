e067cb6d59e6e4da6505b6859bc68cb2771f8841
Nikita Zubkov <zubchick@yandex-team.ru>
2015-11-17 22:32:37 +0300

    Dialogs refactoring

--- ybot/modules/dialog.py
+++ ybot/modules/dialog.py
@@ -2,15 +2,17 @@
 from __future__ import absolute_import
 
 import logging
+from functools import wraps
 
 import telegram
 
 from ybot.state import State
-from ybot.events import subscibe
+from ybot.events import subscibe, splitter, Listener
 from ybot.modules import add_command
 from ybot.modules.telegram import bot
 
 log = logging.getLogger(__name__)
+__dialogs = []
 
 
 class Response(object):
@@ -23,12 +25,47 @@ class Response(object):
 
 
 class Dialog(State):
-    def __init__(self, chat_id, name):
-        super(Dialog, self).__init__(chat_id)
-        self.name = name
-
     def key(self, name):
-        return super(Dialog, self).key('.'.join(map(str, [self.name, name])))
+        return super(Dialog, self).key('.'.join(map(str, ['dialog', name])))
+
+
+def wrap_handler(f):
+    @wraps(f)
+    def inner(name, value):
+        state, store_key, value = value
+        dialog = Dialog(value.chat_id)
+
+        try:
+            result = f(state, value)
+        except Exception:
+            log.exception('Exception in dialog handler %s', f)
+            return
+
+        if not isinstance(result, Response):
+            raise TypeError(
+                'Result of handler %s, event %s must be `Response` type, not %s' %
+                (f, name, type(f))
+            )
+
+        if result.text is not None:
+            private = value.chat.type == 'private'
+            force_reply = False if private else result.next is not None
+            msg = _reply(value.chat_id, result.text, force_reply)
+
+            # change store key if we had an update on group chat
+            if not private:
+                store_key = msg.message_id
+
+        if result.next is not None:
+            state = result.state if result.state is not None else {}
+            dialog.set(store_key, (_fname(result.next), state))
+
+    return inner
+
+
+class DialogHandler(Listener):
+    def __init__(self, f):
+        super(DialogHandler, self).__init__(wrap_handler(f))
 
 
 def _fname(f):
@@ -44,80 +81,47 @@ def _reply(chat_id, text, force_reply):
     return bot.sendMessage(chat_id=chat_id, text=text, **kwargs)
 
 
-def get_dialog_dispatcher(conf):
-    def _dispatch(name, value):
-        entry = conf['']
-        dialog_name = entry.command
-        entry_command = entry.command.lower()
+@splitter(['ybot.telegram.message'], check=False)
+def dialog_dispatcher(name, value):
+    dialog = Dialog(value.chat_id)
+    handler_name = None
+    state = {}
+    store_key = ''
 
-        dialog = Dialog(value.chat_id, dialog_name)
+    # trying initial commands
+    text = value.text.lower()
+    for handler_name, cmd in __dialogs:
+        if cmd.lower() in text:
+            yield handler_name, (state, store_key, value)
+            return
 
-        # if it's initial command
-        if entry_command in value.text.lower():
-            state = {}
-            handler_name = ''
-            store_key = ''
-
-        # dialog continuation in group chat
-        elif value.chat.type == 'group':
-            reply_to = value.reply_to_message
-            if reply_to is None:
-                return
-
+    # dialog continuation in group chat
+    if value.chat.type == 'group':
+        reply_to = value.reply_to_message
+        if reply_to is not None:
             store_key = reply_to.message_id
             handler_name, state = dialog.pop(store_key, (None, {}))
 
-        # dialog continuation in private chat
-        elif value.chat.type == 'private':
-            store_key = ''
-            handler_name, state = dialog.pop(store_key, (None, {}))
-        else:
-            return
+    # dialog continuation in private chat
+    elif value.chat.type == 'private':
+        store_key = ''
+        handler_name, state = dialog.pop(store_key, (None, {}))
+    else:
+        log.error('Unexpected chat type %s', value.chat.type)
+        return
 
-        if handler_name is None:
-            return
+    if handler_name is None:
+        return
 
-        handler = conf[handler_name]
-        state = {} if state is None else state
-
-        try:
-            result = handler(state, value)
-        except Exception:
-            log.exception('Exception in dialog handler %s, dialog %s',
-                          handler, dialog_name)
-            return
-
-        if not isinstance(result, Response):
-            raise TypeError('Result of dialog %s, handler %s '
-                            'must be `Response` type, not %s' %
-                            (dialog_name, handler, type(handler)))
-
-        if result.text is not None:
-            private = value.chat.type == 'private'
-            force_reply = False if private else result.next is not None
-            msg = _reply(value.chat_id, result.text, force_reply)
-
-            # change store key if we had an update on group chat
-            if not private:
-                store_key = msg.message_id
-
-        if result.next is not None:
-            dialog.set(store_key, (_fname(result.next), result.state))
-
-    _dispatch.__name__ = conf[''].__name__
-    return _dispatch
+    yield handler_name, (state, store_key, value)
 
 
 def create_dialog(config):
     add_command(config['command'], config['description'])
-    entry = config['entrypoint']
-    entry.command = config['command']
 
-    disp_conf = {'': entry}
+    entry = _fname(config['entrypoint'])
+    __dialogs.append((entry, config['command']))
+    subscibe(entry, DialogHandler(config['entrypoint']), _run=False)
 
     for handler in config['handlers']:
-        disp_conf[_fname(handler)] = handler
-
-    disp = get_dialog_dispatcher(disp_conf)
-
-    subscibe('ybot.telegram.message', disp, _run=False)
+        subscibe(_fname(handler), DialogHandler(handler), _run=False)
